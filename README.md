8d482051331b0bc726cc172e1e027132b38ddb41
RiverC <riverdalecoder@outlook.com>
2025-10-26 16:34:42 -0400

    im on fire

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReCursor: REcursive Writing Cursor
+A recursive writing assistant, powered by Gemini!
---
+++ ai.py
@@ -0,0 +1,20 @@
+import asyncio
+from gemini_webapi import GeminiClient
+from gemini_webapi.constants import Model
+import os
+Secure_1PSID = os.getenv("GOOG_SID")
+Secure_1PSIDTS = os.getenv("GOOG_TS") 
+
+class AI:
+    def __init__(self,model):
+        self.client = GeminiClient(
+            model=model,
+            secure_1PSID=Secure_1PSID,
+            secure_1PSIDTS=Secure_1PSIDTS,
+        )
+        asyncio.run(self.init())
+    async def init(self):
+        await self.client.init(timeout=30, auto_close=False, close_delay=300, auto_refresh=True)
+    async def generate(self, prompt)-> str:
+        response = await self.client.generate_content(prompt)
+        return response.text
